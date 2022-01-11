7323081c054c7f98e410479eb497cc5a7453194f
Shaun Halverson <85133081+ShaunHalverson@users.noreply.github.com>
2022-01-11 11:40:22 -0600

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResizingTutorial
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle buttonOriginalRectangle;
+        private Rectangle originalFormSize;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            originalFormSize = new Rectangle(this.Location.X, this.Location.Y, this.Size.Width, this.Size.Height);
+            buttonOriginalRectangle = new Rectangle(button1.Location.X, button1.Location.Y, button1.Width, button1.Height);
+        }
+
+        private void resizeControl(Rectangle r, Control c)
+        {
+            float xRatio = (float)(this.Width) / (float)(originalFormSize.Width);
+            float yRatio = (float)(this.Height) / (float)(originalFormSize.Height);
+
+            int newX = (int)(r.Width * xRatio);
+            int newY = (int)(r.Height * yRatio);
+
+            int newWidth = (int)(r.Width * xRatio);
+            int newHeight = (int)(r.Height * yRatio);
+
+            c.Location = new Point(newX, newY);
+            c.Size = new Size(newWidth, newHeight);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resizeControl(buttonOriginalRectangle, button1);
+        }
+    }
+}
